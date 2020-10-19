280de9dd3c54b05ef6ec8f679adaa1648dcf7cba
Vladimir Pochtarenko <wooldik94@gmail.com>
2020-10-19 19:52:58 +0300

    rename classes and style div

--- boom/src/App.js
+++ boom/src/App.js
@@ -4,10 +4,10 @@ import classes from './App.module.css';
 export default class App extends Component {
 	render() {
 		return (<div>
-			<h1 className='text'>Начинай игру!</h1>
-			<button className='btn1' >Кнопка 1</button>
-			<button className='btn2'>Кнопка 2</button>
-			<button className='btn3'>Кнопка 3</button>
+			<h1 className={classes.text}>Начинай игру!</h1>
+			<button className={classes.btn1}>Кнопка 1</button>
+			<button className={classes.btn2}>Кнопка 2</button>
+			<button className={classes.btn3}>Кнопка 3</button>
 		</div>
 		);
 	}
