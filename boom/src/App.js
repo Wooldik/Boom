b6f82b114caa75a712d0f5d364ca56cd249d3103
Vladimir Pochtarenko <wooldik94@gmail.com>
2020-10-19 20:42:15 +0300

    add state

--- boom/src/App.js
+++ boom/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import classes from './App.module.css';
+import './App.css';
 
 export default class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			onBoom: 'red',
+			btn1: 0,
+			btn2: 0,
+			btn3: 0,
+		}
+
+	}
+	randomBoomb = () => {
+		const btnAll = document.getElementsByClassName('btn');
+		console.log(btnAll);
+		this.setState({
+			onBoom: this.state.onBoom === 'red' ? 'green' : 'red',
+		})
+	}
+
 	render() {
+
 		return (<div>
-			<h1 className={classes.text}>Начинай игру!</h1>
-			<button className={classes.btn1}>Кнопка 1</button>
-			<button className={classes.btn2}>Кнопка 2</button>
-			<button className={classes.btn3}>Кнопка 3</button>
+			<h1 className='text'>{this.state.onBoom === 'red' ? 'Ты взорвался!' : 'Ты выйграл!'}</h1>
+			<button className='btn' onClick={this.randomBoomb}>Кнопка 1</button>
+			<button className='btn' onClick={this.randomBoomb}>Кнопка 2</button>
+			<button className='btn' onClick={this.randomBoomb}>Кнопка 3</button>
 		</div>
 		);
 	}
